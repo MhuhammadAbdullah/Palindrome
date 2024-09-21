ca83d38e74f33f3a105e562910c8d397a7641fbc
Muhammad Abdullah <abaloch0033@gmail.com>
2024-09-21 15:01:52 +0500

    updated

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// With Method
+
+
 // var userInput = prompt("Enter a word!");
 
 // if (userInput) {
@@ -13,54 +16,23 @@
 
 
 
-// var word = prompt("Enter the name");
-// var lowerCaseWord = word.toLowerCase();
-
-// var reversedWord = lowerCaseWord.split('').reverse().join('');
-// if ((reversedWord == lowerCaseWord)) {
-//     console.log(`${word} is a palindrome.`);
-// } else {
-//     console.log(`${word} is not a palindrome.`);
-// }
+// Without Method
 
 
-var userInput = prompt("Enter a word!")
-var reversedWord = [];
-var j = 0;
-for (var i = userInput.length - 1; i >= 0; i--) {
-    reversedWord[i] = userInput[j++]
-}
-if (userInput[0] == reversedWord[0]) {
-    alert("Pallindrome")
-} else {
-    alert("Not a Pallindrome")
-}
-// var t=prompt("Enter the word");
-// var a=[];
-// var j=0;
-// for(var i=t.length-1;i>=0;i--){
-//         a[j++]=t[i]
-// }
-// console.log(a);
-// var na=[];
-// for(var i=0;i<t.length;i++){
-//     na[i]=t[i];
-// }
-// var flag=false;
-// console.log(na);
-// var si=0;
-// var ei=t.length-1;
-// if(a[si]==na[si]){
-//     flag=true;
-//     si++;
-// }
-// else{
-//     flag=false;
+// var userInput = prompt("Enter a word!");
+// var reversed = "";
+
+// if (userInput) {
+//     for (var i = userInput.length - 1; i >= 0; i--) {
+//         reversed += userInput[i];
+//     }
+    
+//     if (userInput === reversed) {
+//         alert("The string is a palindrome!");
+//     } else {
+//         alert("The string is not a palindrome.");
+//     }
 // }
 
-// if(flag==true){
-//     console.log("Pallindrome");
-// }
-// else{
-//     console.log("Not aPallindrome");
-// }
+
+
